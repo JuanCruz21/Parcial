cb6b839e7935630d7a7ea1269d49a3ede81df31c
Juan Manuel Rincon Cruz <82663723+JuanCruz21@users.noreply.github.com>
2024-01-17 18:59:21 -0500

    Update Connection.js

--- db/Connection.js
+++ db/Connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-let conexion = 'mongodb+srv://Admin:Admin123@cluster0.6xnee4i.mongodb.net/'
+let conexion = 'url mongo'
 
 mongoose.connect(conexion)
 .then(event=>console.log('Conexion Exitosa'))
